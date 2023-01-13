91eee7111fe48b8217be7f8913b67d76d59c8384
David McGuffey <dmcguffey94@gmail.com>
2023-01-12 23:06:26 -0600

    exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your favorite subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "art":
+                    Console.WriteLine("I love to draw!");
+                    break;
+                case "math":
+                    Console.WriteLine("You must love numbers!");
+                    break;
+                case "science":
+                    Console.WriteLine("That's too broad! You must like them all!");
+                    break;
+                case "gym":
+                    Console.WriteLine("There's nothing like feeling the burn!");
+                    break;
+                case "english":
+                    Console.WriteLine("I love a good book!");
+                    break;
+                default:
+                    Console.WriteLine("That's a new one! I'll have to look into it.");
+                    break;
+            }
         }
     }
 }
